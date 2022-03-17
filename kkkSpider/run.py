d01b1e66c4acb340664de688ba65e42e020b4db5
dudulin <https://github.com/dudulin/webpack4>
2022-03-17 13:34:52 +0800

    * # ctrl + shift + F10   运行当前文件

--- kkkSpider/run.py
+++ kkkSpider/run.py
@@ -2,3 +2,4 @@ from scrapy import cmdline
 
 cmdline.execute('scrapy crawl kkk -o kkk.json'.split())
 # cmdline.execute('scrapy crawl kkk2 -o kkk2.json'.split())  # 运行模式
+# ctrl + shift + F10   运行当前文件
